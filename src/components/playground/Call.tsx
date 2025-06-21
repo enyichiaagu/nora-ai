a184fda3c2a5ba2deab28591bcea71f076544bae
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-21 10:39:20 +0000

    Updated Call.tsx

--- src/components/playground/Call.tsx
+++ src/components/playground/Call.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { DailyAudio, DailyVideo, useCallObject } from '@daily-co/daily-react';
+import { DailyAudio, DailyVideo, useLocalSessionId, useCallObject } from '@daily-co/daily-react';
 import { ConversationData } from './types/conversation';
 import EndCall from './EndCall'
 import Static from './Static'
@@ -10,14 +10,15 @@ interface CallProps {
 
 const Call: React.FC<CallProps> = ({ data }) => {
   const [callObject, setCallObject] = useState(useCallObject())
+  const localSession = useLocalSessionId()
   
   return (
     <div 
       className="w-full h-[600px] bg-white/10 backdrop-blur-sm rounded-lg border border-white/20 shadow-xl overflow-hidden relative"
     >
+      <DailyVideo />
+      <DailyVideo automirror />
       <EndCall />
-      <DailyVideo />
-      <DailyVideo />
       <DailyAudio onPlayFailed={(e) => console.error(`Failed to play ${e.type} for ${e.sessionId}.`)}/>
     </div>
   );
