97ba6edd22edec64d71b16493647df758d6c7f2c
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-21 10:32:42 +0000

    Updated Call.tsx

--- src/components/playground/Call.tsx
+++ src/components/playground/Call.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { useCallObject } from '@daily-co/daily-react';
+import { DailyAudio, useCallObject } from '@daily-co/daily-react';
 import { ConversationData } from './types/conversation';
 import EndCall from './EndCall'
 import Static from './Static'
@@ -9,11 +9,14 @@ interface CallProps {
 }
 
 const Call: React.FC<CallProps> = ({ data }) => {
+  // const [callObject, setCallObject] = useCallObject()
+  
   return (
     <div 
       className="w-full h-[600px] bg-white/10 backdrop-blur-sm rounded-lg border border-white/20 shadow-xl overflow-hidden relative"
     >
       <EndCall />
+      <DailyAudio onPlayFailed={(e) => console.error(`Failed to play ${e.type} for ${e.sessionId}.`)}/>
     </div>
   );
 };
