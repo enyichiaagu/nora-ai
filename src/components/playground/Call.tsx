38f9c502b2aae14630b457d65ff58126d3709e02
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-21 10:19:34 +0000

    Updated Call.tsx

--- src/components/playground/Call.tsx
+++ src/components/playground/Call.tsx
@@ -9,30 +9,6 @@ interface CallProps {
 }
 
 const Call: React.FC<CallProps> = ({ data }) => {
-  const [callState, setCallState] = useState(STATE_IDLE)
-  const [callObject, setCallObject] = useState()
-
-  useEffect(() => {
-    const newCallObject = useCallObject()
-    setCallObject(newCallObject)
-    newCallObject.startCamera()
-    newCallObject.join({url: data.conversation_url, userName: 'You'})
-  }, [data?.conversation_url])
-
-  const leaveCall = useCallback(() => {
-    if (!callObject) return;
-
-    
-  }, [callObject, callState])
-
-  
-  
-  if (!data) {
-    return (
-      <Static/>
-    );
-  }
-
   return (
     <div 
       className="w-full h-[600px] bg-white/10 backdrop-blur-sm rounded-lg border border-white/20 shadow-xl overflow-hidden relative"
