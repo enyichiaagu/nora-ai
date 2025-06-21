bac4b703d59a9c3dc725119d676d8aac519aafd1
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-21 10:48:30 +0000

    Updated Call.tsx

--- src/components/playground/Call.tsx
+++ src/components/playground/Call.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { DailyAudio, DailyVideo, useLocalSessionId, useCallObject } from '@daily-co/daily-react';
+import { DailyAudio, DailyVideo, useParticipantIds, useLocalSessionId, useCallObject } from '@daily-co/daily-react';
 import { ConversationData } from './types/conversation';
 import EndCall from './EndCall'
 import Static from './Static'
@@ -11,13 +11,14 @@ interface CallProps {
 const Call: React.FC<CallProps> = ({ data }) => {
   const [callObject, setCallObject] = useState(useCallObject())
   const localSession = useLocalSessionId()
+  const remoteParticipantIds = useParticipantIds({ filter: 'remote' });
   
   return (
     <div 
       className="w-full h-[600px] bg-white/10 backdrop-blur-sm rounded-lg border border-white/20 shadow-xl overflow-hidden relative"
     >
-      <DailyVideo />
-      <DailyVideo automirror />
+      <DailyVideo automirror sessionId={localSession}/>
+      {remoteParticipantIds.length > 0 && <DailyVideo sessionId={remoteParticipantIds}/>}
       <EndCall />
       <DailyAudio onPlayFailed={(e) => console.error(`Failed to play ${e.type} for ${e.sessionId}.`)}/>
     </div>
