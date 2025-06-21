f8142a90ad7b2821689df363b11558dd63e1289a
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-21 10:20:26 +0000

    Updated index.tsx

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -8,6 +8,7 @@ import useCall from './hooks/useCall';
 
 const Playground: React.FC = () => {
   const [apiKey, setApiKey] = useState<string>('');
+  const [callObject, setCallObject] = useState(null);
   const { data, loading, error, makeCall } = useCall();
 
   const handleStart = useCallback(() => {
