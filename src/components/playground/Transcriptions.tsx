b61650a8e572e9edb5d59c51cbe04add4f2ab433
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-21 13:44:33 +0000

    Updated Transcriptions.tsx

--- src/components/playground/Transcriptions.tsx
+++ src/components/playground/Transcriptions.tsx
@@ -1,2 +1,11 @@
-function Transcriptions() {  
+import {useState} from 'react'
+import { useTranscription } from '@daily-co/daily-react';
+
+function Transcriptions() {
+  const [transcript, setTranscript] = useState('')
+  const {startTranscription, stopTranscription} = useTranscription({
+    onTransriptionMessage: (message) => {
+      
+    }
+  })
 }
