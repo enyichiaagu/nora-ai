b3bf5d550dea7851bfcf685f04d0512d06372e99
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-21 07:41:13 +0000

    Updated Call.tsx

--- src/components/playground/Call.tsx
+++ src/components/playground/Call.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState, useEffect } from 'react';
 import DailyIframe from '@daily-co/daily-js';
 import { ConversationData } from './types/conversation';
 import { Button } from '@/components/ui/button';
-import { PhoneOff, Video, Users } from 'lucide-react';
+import { PhoneOff, Users } from 'lucide-react';
 import LoadingSpinner from '@/components/common/LoadingSpinner';
 import Static from './Static'
 
