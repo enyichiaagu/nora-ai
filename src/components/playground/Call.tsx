5f8d560bf8b916f9a61ecd4c77d77f50363feba5
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-21 14:21:02 +0000

    Updated Call.tsx

--- src/components/playground/Call.tsx
+++ src/components/playground/Call.tsx
@@ -23,7 +23,7 @@ const Call: React.FC<CallProps> = ({ data, onCallEnd }) => {
   const localSessionId = useLocalSessionId();
   const remoteParticipantIds = useParticipantIds({ filter: 'remote' });
   const [isEnding, setIsEnding] = useState(false);
-  const { transcript, startTranscription, stopTranscription } = useTranscript()
+  const { transcript } = useTranscript()
 
   useEffect(() => {
     if (!callObject || !data?.conversation_url || isEnding) return;
