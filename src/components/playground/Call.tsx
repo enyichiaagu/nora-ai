f647ddd5fdc389ac6d10c581bbb8fef93d987ed0
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-21 07:58:03 +0000

    Updated Call.tsx

--- src/components/playground/Call.tsx
+++ src/components/playground/Call.tsx
@@ -195,21 +195,7 @@ const Call: React.FC<CallProps> = ({ data, onCallEnd }) => {
 
       {/* Animated end call button */}
       {Object.keys(participants).length > 0 && (
-        <div 
-          className={`absolute bottom-6 left-1/2 transform -translate-x-1/2 transition-all duration-300 ease-in-out ${
-            isHovered ? 'translate-y-0 opacity-100' : 'translate-y-16 opacity-0'
-          }`}
-        >
-          <Button 
-            onClick={endCall}
-            variant="destructive"
-            size="lg"
-            className="shadow-lg hover:shadow-xl transition-shadow duration-200"
-          >
-            <PhoneOff className="h-5 w-5" />
-            End Call
-          </Button>
-        </div>
+        <EndCall />
       )}
     </div>
   );
