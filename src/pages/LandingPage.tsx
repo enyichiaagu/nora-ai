20621eda4ff53c0ccda6b688862200de9b6c5bc1
Sebastian Anioke <aniokechukwudi7@gmail.com>
2025-06-22 23:18:13 +0100

    Updated LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Header from '@/components/landing-page/Header';
 import Hero from '@/components/landing-page/Hero';
 import Mission from '@/components/landing-page/Mission';
