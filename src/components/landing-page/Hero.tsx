7a21dd1b1e429f0e347b860368cfea18e53a7140
Sebastian Anioke <aniokechukwudi7@gmail.com>
2025-06-23 00:40:01 +0100

    Add h1, p, and buttons to Hero component

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Hero: React.FC = () => {
   return (
     <section className="h-[90vh] bg-app-primary noice">
-     
+      <h1>Learn Like You're Chatting with a Smart Friend.</h1>
+      <p>AI-powered video call, real-time transcription, and personalized support that makes learning experience more relaxed and enjoyable</p>
+      <div>
+        <button>Get Started</button>
+        <button>Learn More</button>
+      </div>
     </section>
   );
 };
