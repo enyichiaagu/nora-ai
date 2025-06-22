e4744093f9343c4a4939f746fef24c8cf1cdf664
Sebastian Anioke <aniokechukwudi7@gmail.com>
2025-06-22 18:59:17 +0100

    Updated Header.tsx

--- src/components/landing-page/Header.tsx
+++ src/components/landing-page/Header.tsx
@@ -3,18 +3,22 @@ import { LogOut } from 'lucide-react';
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-2">
-          <img src="/icons/logo.svg" alt="Nora AI" className="w-8 h-8" />
-          <p className="text-lg font-semibold">Nora AI</p>
-        </div>
-        <button className="flex items-center gap-2 px-4 py-2 bg-app-primary text-white rounded-lg hover:bg-blue-600 transition-colors">
-          <LogOut className="w-4 h-4" />
-          Sign In
-        </button>
-      </div>
-    </header>
+  	<header className='absolute flex items-center justify-between max-w-6xl w-full top-[8%] left-1/2 -translate-x-1/2'>
+			<div className='flex items-center gap-2 '>
+				<img
+					src='/icons/logo.svg'
+					alt=''
+					className='h-[1.7rem] mb-2'
+				/>
+				<p className='text-white font-montserrat text-xl '>Nora AI</p>
+			</div>
+			<div>
+				<button className='bg-white text-app-primary rounded-lg px-6 py-2 shadow-lg flex items-center justify-center gap-2'>
+					Sign In
+					<LogOut className='w-4' />
+				</button>
+			</div>
+		</header>
   );
 };
 
