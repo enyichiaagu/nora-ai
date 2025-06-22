b37d66e0121d04e1a5a84b75db35493666b4e2db
Sebastian Anioke <aniokechukwudi7@gmail.com>
2025-06-22 18:52:48 +0100

    Create LandingPage component

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Header from '@/components/landing-page/Header';
+import Hero from '@/components/landing-page/Hero';
+import Mission from '@/components/landing-page/Mission';
+import Features from '@/components/landing-page/Features';
+import Pricing from '@/components/landing-page/Pricing';
+import Faq from '@/components/landing-page/Faq';
+import Cta from '@/components/landing-page/Cta';
+import Footer from '@/components/landing-page/Footer';
+
+const LandingPage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Mission />
+      <Features />
+      <Pricing />
+      <Faq />
+      <Cta />
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
