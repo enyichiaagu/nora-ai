2a222921f31b0fa2a2208b1082e82ab4719fafa0
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-22 11:01:09 +0000

    Updated useTranscript.tsx

--- src/components/playground/hooks/useTranscript.tsx
+++ src/components/playground/hooks/useTranscript.tsx
@@ -1,7 +1,8 @@
-import {useState} from 'react'
+import {useState, useRef} from 'react'
 
 export default function useTranscript(){
   const [transcript, setTranscript] = useState('')
+  const recordRef = useRef() // 
   
 
   return { transcript, isTranscribing, startTranscription, stopTranscription }
