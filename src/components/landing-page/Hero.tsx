492427e95cc03437cc94224c5d1cdb56554a2a48
Sebastian Anioke <aniokechukwudi7@gmail.com>
2025-06-23 00:42:35 +0100

    Updated Hero.tsx

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -3,12 +3,37 @@ import React from 'react';
 const Hero: React.FC = () => {
   return (
     <section className="h-[90vh] bg-app-primary noice">
-      <h1>Learn Like You're Chatting with a Smart Friend.</h1>
-      <p>AI-powered video call, real-time transcription, and personalized support that makes learning experience more relaxed and enjoyable</p>
       <div>
-        <button>Get Started</button>
-        <button>Learn More</button>
-      </div>
+				<div className='mx-auto w-fit mb-4'>
+					<img
+						src='/icons/award.svg'
+						className='w-16'
+						alt=''
+					/>
+				</div>
+				<h1 className='text-[3rem] font-marlin font-semibold text-center text-white'>
+					Learn Like You're Chatting <br className='hidden md:block' />
+					with a Smart Friend.
+				</h1>
+				<p className='font-bricolage text-gray-100 w-[55%] mx-auto text-center mt-3 text-lg'>
+					AI-powered video calls, real-time transcription, and personalized
+					support that makes learning experience more relaxed and
+					enjoyable.
+				</p>
+				<div className='flex justify-center items-center gap-4 mt-9 font-montserrat'>
+					<button className='flex justify-center items-center gap-2 bg-white text-gray-800 px-5 py-3 rounded-md shadow-md'>
+						Get Started{" "}
+						<img
+							src='/icons/right-arrow.svg'
+							alt=''
+							className='w-5'
+						/>
+					</button>
+					<button className='border  px-8 py-3 rounded-md text-white flex justify-center items-center gap-2'>
+						Learn More
+					</button>
+				</div>
+			</div>
     </section>
   );
 };
