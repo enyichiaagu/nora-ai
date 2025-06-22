aab5d25312e4344fad89dcc7d0256f2fd487e0ff
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-22 11:24:48 +0000

    Updated useTranscript.tsx

--- src/components/playground/hooks/useTranscript.tsx
+++ src/components/playground/hooks/useTranscript.tsx
@@ -2,7 +2,7 @@ import {useState, useRef} from 'react'
 
 export default function useTranscript(){
   const [transcript, setTranscript] = useState('')
-  // Inside this hook, start mediarecorder once a function startTranscribing is called, then export that function. It will be called on the start audio button in ../index.tsx. Then console.log the blob of audio every three seconds. 
+  // Inside this hook, start mediarecorder once a function startTranscribing is called, then export that function. It will be called on the start audio button in ../index.tsx. Then console.log the blob of audio gotten every three seconds. export an audio 'stopTranscribing' that just stops the recording.
   
 
   return { transcript }
