cdf6ec5fe664721bb532ce5f2a8a7471ed2921d6
Sebastian Anioke <aniokechukwudi7@gmail.com>
2025-06-23 00:37:02 +0100

    Updated Hero.tsx

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero: React.FC = () => {
   return (
     <section className="h-[90vh] bg-app-primary noice">
-      <h1>Hero Component</h1>
+     
     </section>
   );
 };
