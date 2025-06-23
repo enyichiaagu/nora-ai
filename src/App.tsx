1df2be1e7c2e82dad5ecdd8b2d08b8efc2d153c4
Sebastian Anioke <aniokechukwudi7@gmail.com>
2025-06-23 14:46:06 +0100

    Add Dashboard and Onboarding routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './app.css';
 import Waitlist from './pages/Waitlist';
 import Demo from './pages/Demo';
 import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Dashboard';
+import Onboarding from './pages/Onboarding';
 import { Toaster } from 'react-hot-toast';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 
@@ -17,6 +19,14 @@ const router = createBrowserRouter([
   {
     path: '/dev',
     element: <LandingPage />
+  },
+  {
+    path: '/dashboard',
+    element: <Dashboard />
+  },
+  {
+    path: '/onboarding',
+    element: <Onboarding />
   }
 ]);
 
