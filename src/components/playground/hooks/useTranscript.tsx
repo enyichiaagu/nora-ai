d8287332ba8f58198c5833882c9ff203f7031746
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-23 11:58:11 +0100

    Updated useTranscript.tsx

--- src/components/playground/hooks/useTranscript.tsx
+++ src/components/playground/hooks/useTranscript.tsx
@@ -12,7 +12,6 @@ export default function useTranscript() {
     const websocket = new WebSocket('wss://467b-102-90-118-228.ngrok-free.app');
     websocket.onmessage = (event) => {
      const data = JSON.parse(event.data);
-      console.log(data.transcript)
      // setTranscript(data.transcript)
     };
     websockRef.current = websocket;
