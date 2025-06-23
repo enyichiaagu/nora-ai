248467335dc10f96022db688ded7d6a1e0ff5c22
Sebastian Anioke <aniokechukwudi7@gmail.com>
2025-06-23 06:34:22 +0100

    Updated Hero.tsx

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -1,25 +1,25 @@
-const Hero: React.FC = () => {
-  return (
-  <section className='bg-app-primary noice min-h-screen md:min-h-[40rem] flex items-center justify-center px-4'>
-      <div className='w-full max-w-4xl'>
+function Hero() {
+	return (
+		<section className='bg-app-primary noice md:min-h-[40rem] h-[90vh] flex items-center justify-center '>
+			<div>
 				<div className='mx-auto w-fit mb-4'>
 					<img
-						src='/icons/award.svg'
-						className='w-12 md:w-16'
+						src='/icons/star.svg'
+						className='w-16'
 						alt=''
 					/>
 				</div>
-				<h1 className='text-2xl sm:text-3xl md:text-4xl lg:text-[3rem] font-marlin font-semibold text-center text-white leading-tight'>
-					Learn Like You're Chatting <br className='hidden sm:block' />
+				<h1 className='text-[3rem] font-marlin font-semibold text-center text-white'>
+					Learn Like You're Chatting <br className='hidden md:block' />
 					with a Smart Friend.
 				</h1>
-				<p className='font-bricolage text-gray-100 w-full sm:w-[80%] md:w-[55%] mx-auto text-center mt-3 text-base md:text-lg px-4 md:px-0'>
+				<p className='font-bricolage text-gray-100 w-[55%] mx-auto text-center mt-3 text-lg'>
 					AI-powered video calls, real-time transcription, and personalized
 					support that makes learning experience more relaxed and
 					enjoyable.
 				</p>
-				<div className='flex flex-col sm:flex-row justify-center items-center gap-4 mt-9 font-montserrat px-4 md:px-0'>
-					<button className='w-full sm:w-auto flex justify-center items-center gap-2 bg-white text-gray-800 px-5 py-3 rounded-md shadow-md'>
+				<div className='flex justify-center items-center gap-4 mt-9 font-montserrat'>
+					<button className='flex justify-center items-center gap-2 bg-white text-gray-800 px-5 py-3 rounded-md shadow-md'>
 						Get Started{" "}
 						<img
 							src='/icons/right-arrow.svg'
@@ -27,13 +27,13 @@ const Hero: React.FC = () => {
 							className='w-5'
 						/>
 					</button>
-					<button className='w-full sm:w-auto border px-8 py-3 rounded-md text-white flex justify-center items-center gap-2'>
+					<button className='border  px-8 py-3 rounded-md text-white flex justify-center items-center gap-2'>
 						Learn More
 					</button>
 				</div>
 			</div>
-    </section>
-  );
-};
+		</section>
+	);
+}
 
 export default Hero;
