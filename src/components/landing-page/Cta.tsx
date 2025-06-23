070ea91cfe3999ce96ea96106d27de8000e298d6
Sebastian Anioke <aniokechukwudi7@gmail.com>
2025-06-23 08:03:18 +0100

    Updated Cta.tsx

--- src/components/landing-page/Cta.tsx
+++ src/components/landing-page/Cta.tsx
@@ -4,9 +4,9 @@ const Cta: React.FC = () => {
   return (
     <section className='max-w-6xl mx-auto my-[8rem] px-4 '>
       <div className='relative bg-gradient-to-br from-app-primary via-blue-600 to-blue-800 rounded-3xl overflow-hidden '>
-         <img 
-                    src="/images/noise.png"
-                    className='w-full h-full filter absolute object-contain'
+         <div 
+                    
+                    className='w-full h-full filter absolute object-contain noice'
                   />
         {/* Background Pattern */}
         <div className='absolute inset-0 noice opacity-30'></div>
