a9cd7f3042d18fca5c5da1fd276adcc6e766dd96
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-23 11:49:04 +0100

    Updated useTranscript.tsx

--- src/components/playground/hooks/useTranscript.tsx
+++ src/components/playground/hooks/useTranscript.tsx
@@ -12,12 +12,10 @@ export default function useTranscript() {
 
   const startTranscribing = async () => {
     const websocket = new WebSocket('wss://467b-102-90-118-228.ngrok-free.app');
-    websocket.onopen = async () => {
-      websocket.onmessage = (event) => {
-       const data = JSON.parse(event.data);
-       setTranscript(data.transcript)
-      };
-    }
+    websocket.onmessage = (event) => {
+     const data = JSON.parse(event.data);
+     setTranscript(data.transcript)
+    };
     websockRef.current = websocket;
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true })
