386aeaf5971b74ee347ec3eb3a71968a12d98cf9
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-23 16:45:27 +0100

    Updated Static.tsx

--- src/components/playground/Static.tsx
+++ src/components/playground/Static.tsx
@@ -3,7 +3,7 @@ import { Video, Users } from 'lucide-react'
 function Static() {
   return (
     <div 
-        className="w-full h-[600px] bg-white/10 backdrop-blur-sm rounded-lg border border-white/20 p-8 flex flex-col items-center justify-center shadow-lg"
+        className="w-full h-[600px] flex flex-col items-center justify-center shadow-lg"
       >
       <div className="bg-white/20 backdrop-blur-sm rounded-full p-6 shadow-md mb-4">
         <Video className="h-12 w-12 text-white" />
