094bc6a263629062db5357405f6f36017af5d71b
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-23 16:47:55 +0100

    Updated Static.tsx

--- src/components/playground/Static.tsx
+++ src/components/playground/Static.tsx
@@ -1,7 +1,7 @@
 import { Video, Users } from 'lucide-react'
 
 function Static() {
-  return (
+  return (<>
       <div className="bg-white/20 backdrop-blur-sm rounded-full p-6 shadow-md mb-4">
         <Video className="h-12 w-12 text-white" />
       </div>
@@ -12,7 +12,7 @@ function Static() {
       <div className="flex items-center gap-2 mt-4 text-sm text-white/70">
         <Users className="h-4 w-4" />
         <span>Waiting for participants...</span>
-      </div>
+      </div></>
   )
 }
 
