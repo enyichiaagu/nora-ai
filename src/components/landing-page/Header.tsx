40738c30caa092d1d54736ef2d3794f838fdae41
Sebastian Anioke <aniokechukwudi7@gmail.com>
2025-06-24 10:39:56 +0100

    Updated Header.tsx

--- src/components/landing-page/Header.tsx
+++ src/components/landing-page/Header.tsx
@@ -2,19 +2,21 @@ import { LogOut } from "lucide-react";
 
 function Header() {
 	return (
-		<header className='absolute flex items-center justify-between max-w-6xl w-full top-[8%] left-1/2 -translate-x-1/2'>
-			<div className='flex items-center gap-2 '>
+		<header className='absolute flex items-center justify-between w-full max-w-6xl .top-[6%] top-[8vh]  .md:top-[8%] left-1/2 -translate-x-1/2 px-4 md:px-6 lg:px-0 z-20'>
+			<div className='flex items-center gap-1'>
 				<img
 					src='/icons/logo.svg'
-					alt=''
-					className='h-[1.7rem] mb-2'
+					alt='Nora AI Logo'
+					className='h-[1.4rem] md:h-[1.7rem] mb-2'
 				/>
-				<p className='text-white font-montserrat text-xl '>Nora AI</p>
+				<p className='text-white font-montserrat font-normal text-xl '>
+					Nora AI
+				</p>
 			</div>
 			<div>
-				<button className='bg-white text-app-primary rounded-lg px-6 py-2 shadow-lg flex items-center justify-center gap-2'>
-					Sign In
-					<LogOut className='w-4' />
+				<button className='bg-white text-app-primary rounded-lg px-4 py-2 md:px-6 md:py-2 shadow-lg flex items-center justify-center gap-2 text-sm md:text-base font-medium transition-all hover:shadow-xl'>
+					<span className='inline'>Sign In</span>
+					<LogOut className='w-5' />
 				</button>
 			</div>
 		</header>
