828ae0f99e0dfabfdc69f03e01082fcdb127e5e1
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-26 07:58:07 +0100

    Updated useTranscript.tsx

--- src/components/playground/hooks/useTranscript.tsx
+++ src/components/playground/hooks/useTranscript.tsx
@@ -54,7 +54,7 @@ export default function useTranscript(audioTrack: MediaStreamTrack | undefined):
         // Clear buffers
         bufferQueue.current = [];
         bufferSize.current = 0;
-      }, 300)
+      }, 50)
       intervalIdRef.current = intervalId
 
       websocket.onmessage = (event: MessageEvent) => {
