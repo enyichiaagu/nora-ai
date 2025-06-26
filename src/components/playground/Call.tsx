d624d9c8162d01a0bb1033ad1a5d8b149c4110bb
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-26 22:35:45 +0100

    Updated Call.tsx

--- src/components/playground/Call.tsx
+++ src/components/playground/Call.tsx
@@ -27,7 +27,7 @@ const Call: React.FC<CallProps> = ({ data, onCallEnd }) => {
   const remoteTrack = useAudioTrack(remoteParticipantIds?.[0]);
   const localTrack = useAudioTrack(localSessionId);
   const { isRecording, transcript, startTranscribing, stopTranscribing } =
-    useTranscript([audioTrack?.persistentTrack, localTrack?.persistentTrack]);
+    useTranscript([remoteTrack?.persistentTrack, localTrack?.persistentTrack]);
 
   useEffect(() => {
     if (!callObject || !data?.conversation_url || isEnding) return;
