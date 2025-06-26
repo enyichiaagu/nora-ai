124322cbcf7fc6297fab34e26ab1fcc1caa4d0cd
Amazing Enyichi Agu <enyichiaagu@gmail.com>
2025-06-26 16:55:13 +0100

    Updated useTranscript.tsx

--- src/components/playground/hooks/useTranscript.tsx
+++ src/components/playground/hooks/useTranscript.tsx
@@ -20,36 +20,8 @@ export default function useTranscript(
   const bufferSize = useRef(0);
 
   const startTranscribing = async (): Promise<void> => {
-    try {
-      if (!audioTrack)
-        throw new Error('Cannot start transcription without remote audio');
-      setTranscript('Transcription Starting ...');
-
-      // Will soon set up Backend to have a permanent ws endpoint
-      const websocket = new WebSocket(
-        'wss://nora-backend-kjwh.onrender.com'
-      );
-      websockRef.current = websocket;
-
-      const ctx = new AudioContext({ sampleRate: 16_000 });
-      await ctx.audioWorklet.addModule('/scripts/audioworklet.js');
-      const source = ctx.createMediaStreamSource(new MediaStream([audioTrack]));
-      const pcmNode = new AudioWorkletNode(ctx, 'pcm-processor');
-      source.connect(pcmNode);
-      pcmNode.connect(ctx.destination);
-
-      pcmNode.port.onmessage = (e) => {
-        if (e.data) {
-          bufferQueue.current.push(new Uint8Array(e.data));
-          bufferSize.current += e.data.byteLength;
-        }
-      };
-
       
       setIsRecording(true);
-    } catch (error) {
-      console.error('Error Starting Transcription:', error);
-    }
   };
 
   const stopTranscribing = (): void => {
