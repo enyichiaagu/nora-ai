009765f4d281956780e2b4ca461bd9724992577f
enyichiaagu <enyichiaagu@gmail.com>
2025-09-24 15:54:22 +0100

    fix: changed Sign In button to  link

--- src/components/landing-page/Header.tsx
+++ src/components/landing-page/Header.tsx
@@ -1,10 +1,7 @@
 import { LogOut } from "lucide-react";
-// import { useEffect } from "react";
-import { useNavigate } from "react-router";
+import { Link } from "react-router";
 
 function Header() {
-	const naviagate = useNavigate();
-
 	return (
 		<header className='absolute flex items-center justify-between w-full max-w-6xl  md:top-[8vh] top-[5vh]  left-1/2 -translate-x-1/2 px-4 md:px-6 lg:px-0 z-20'>
 			<div className='flex items-center gap-1'>
@@ -18,13 +15,13 @@ function Header() {
 				</p>
 			</div>
 			<div>
-				<button
-					onClick={() => naviagate("/auth")}
+				<Link
+					to='/auth'
 					className='bg-white text-app-primary font-marlin rounded-lg px-3 py-2 md:px-6 md:py-2 shadow-lg flex items-center justify-center gap-2 text-sm md:text-base font-medium transition-all hover:shadow-xl'
 				>
 					<span className='inline'>Sign In</span>
 					<LogOut className='w-5' />
-				</button>
+				</Link>
 			</div>
 		</header>
 	);
